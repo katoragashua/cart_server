98296fd8554e496518444ad12d5e8daf1bf5a969
Katoragashua <katoragashua@outlook.com>
2026-04-06 05:05:53 +0100

    Cart_server: Added client facing functions to ProductsServer

--- lib/products_server.ex
+++ lib/products_server.ex
@@ -15,19 +15,31 @@ defmodule ProductsServer do
     GenServer.cast(__MODULE__, {:add_product, attrs})
   end
 
+  def get_products do
+    GenServer.call(__MODULE__, :get_products)
+  end
+
+  def get_product(id) do
+    GenServer.call(__MODULE__, {:get_product, id})
+  end
+
   # Server Callbacks
   @impl true
   def init(state) do
     # trap_exit(). Trapping exits is used when you want your process to treat exit signals as messages. This is useful when you want to monitor other processes and react to their termination, or when you want to ensure that your process can perform cleanup tasks before it terminates.
     # This however, doesn't work for :kill terms. :kill terms are untrappable.
     Process.flag(:trap_exit, true)
+
+    # {:ok, state, 5_000} # This is used to set a timeout for the process. If the process doesn't receive any messages within the specified time, it will receive a :timeout message. This is useful for implementing timeouts or for performing periodic tasks.
     {:ok, state}
   end
 
   # handle_call clauses
   @impl true
   def handle_call(:get_products, _from, state) do
-    {:reply, Map.values(state), state}
+    # Simulate a long-running operation
+    :timer.sleep(10_000)
+    {:reply, Map.values(state), state, 5_000}
   end
 
   @impl true
@@ -44,8 +56,12 @@ defmodule ProductsServer do
   # end
 
   @impl true
-  def handle_cast({:add_product, attrs}, state) do # This is a bit more complex than the previous one, we need to generate a product from the attrs and then add it to the state. attrs is a map that contains the product attributes, we need to generate a product from it and then add it to the state. The state is a map that contains the products, we can use the product id as the key and the product struct as the value.
-    product_id = DateTime.utc_now() |> DateTime.to_unix(:microsecond) |> Integer.to_string()
+  # This is a bit more complex than the previous one, we need to generate a product from the attrs and then add it to the state. attrs is a map that contains the product attributes, we need to generate a product from it and then add it to the state. The state is a map that contains the products, we can use the product id as the key and the product struct as the value.
+  def handle_cast({:add_product, attrs}, state) do
+    product_id =
+      DateTime.utc_now()
+      |> DateTime.to_unix(:microsecond)
+      |> Integer.to_string()
 
     product = generate_product_from_attrs(attrs)
     new_state = Map.put(state, product_id, product)
@@ -57,6 +73,12 @@ defmodule ProductsServer do
   @impl true
   def handle_info({:EXIT, from, reason}, state) do
     IO.inspect({from, reason}, label: "ProductsServer EXIT", pretty: true)
+    {:noreply, state}
+  end
+
+  @impl true
+  def handle_info(:yes, state) do
+    IO.puts("You said yes!")
     {:noreply, state}
   end
 
